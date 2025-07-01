06538322e0b4950386e0a477730b0f97de854f51
Jess-mach <machadojessica1997@gmail.com>
2025-07-01 10:41:00 -0300

    Adicionando Pacientes, banco de dados e validação

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,9 +1,14 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
-        String nome, String email,
-        String crm, Especialidade especialidade,
-        DadosEndereco endereco) {
+         @NotBlank String nome,
+         @NotBlank String email,
+         @NotBlank String crm,
+         @Valid @NotNull Especialidade especialidade,
+         @Valid @NotNull DadosEndereco endereco) {
 }
---
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,15 @@
+package med.voll.api.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.medico.Especialidade;
+
+public record DadosCadastroPaciente(
+        @NotBlank String nome,
+        @NotBlank String email,
+        @NotBlank String telefone,
+        @NotBlank String cpf,
+        @Valid @NotNull DadosEndereco endereco) {
+}
