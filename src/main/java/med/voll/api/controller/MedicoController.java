3352e32fd17f6d2834ac20dbe131f19a5b1c6018
Jess-mach <machadojessica1997@gmail.com>
2025-07-02 18:27:51 -0300

    Listagem de medicos com paginação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,14 +3,14 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -28,4 +28,10 @@ public class MedicoController {
 
         return ResponseEntity.created(URI.create("/medicos/" + save.getId())).body(save);
     }
+
+    @GetMapping
+    public Page<DadosListagemMedicos> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedicos :: new);
+    }
+
 }
