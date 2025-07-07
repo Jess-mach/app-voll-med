0c028c83f2baa7e00c75bfb4c20bd32c0014094b
Jess-mach <machadojessica1997@gmail.com>
2025-07-07 10:59:20 -0300

    ""

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
 import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,12 +26,13 @@ public class AutenticacaoController {
 
     @PostMapping
     public ResponseEntity<String> efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(
+        var authenticationtoken = new UsernamePasswordAuthenticationToken(
                 dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authentication = manager.authenticate(authenticationtoken);
 
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
-        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 
 
