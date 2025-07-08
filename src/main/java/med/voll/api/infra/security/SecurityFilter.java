47ff1bac181b048a44313a21549e0e35aea2201f
Jess-mach <machadojessica1997@gmail.com>
2025-07-08 12:11:03 -0300

    Recuperando o token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -8,19 +8,27 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import med.voll.api.infra.exception.AccessDeniedException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal
-            (HttpServletRequest request,
-             HttpServletResponse response,
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
 
-             FilterChain filterChain
-            ) throws ServletException, IOException {
+        System.out.println(tokenJWT);
 
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new AccessDeniedException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+
+        return authorizationHeader.replace("Bearer ", "");
     }
 
 }
